0902a5bc50a143dde2441ba5b52cabbc40d3c391
miguelpardooo <114778564+miguelpardooo@users.noreply.github.com>
2023-11-06 19:31:40 +0100

    Add files via upload

---
+++ Proyecto1_XML_JAVA_DOM/src/proyecto1_xml_java_dom/Proyecto1_XML_JAVA_DOM.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto1_xml_java_dom;
+
+/**
+ *
+ * @author alum
+ */
+public class Proyecto1_XML_JAVA_DOM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
